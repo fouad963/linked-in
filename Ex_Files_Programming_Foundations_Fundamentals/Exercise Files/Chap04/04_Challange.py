4cfbc6a62aed6e6981d5bc03803bc38ef135017c
fouad963 <106271643+fouad963@users.noreply.github.com>
2022-06-01 11:28:44 +0200

    Add files via upload

---
+++ Ex_Files_Programming_Foundations_Fundamentals/Exercise Files/Chap04/04_Challange.py
@@ -0,0 +1,9 @@
+Gussing = input("What's my favorite food:")
+food="burger"
+if food == Gussing:
+    print("Yep! So amazing")
+
+else:
+    print("Yuck! That's not it")
+print("Thanks for playing")
+
